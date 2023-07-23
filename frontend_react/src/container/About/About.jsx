f541e3ebf196695baf4f1f569c636f5867510651
Ahmed Admani <ahmedriaz-50@hotmail.com>
2023-07-23 18:40:36 +0500

    Abouts Page Dynamic Via Sanity

--- frontend_react/src/container/About/About.jsx
+++ frontend_react/src/container/About/About.jsx
@@ -2,21 +2,38 @@ import React, { useState, useEffect } from 'react'
 import { motion } from 'framer-motion'
 
 import './About.scss'
-import {images} from '../../constants'
-const abouts = [
-  { title: 'Web Development', description: 'I am a good developer', imgUrl: images.about01},
-  { title: 'Web Design', description: 'I am a good developer', imgUrl: images.about02},
-  { title: 'UI/UX', description: 'I am a good developer', imgUrl: images.about03},
-  { title: 'Full Stack', description: 'I am a good developer', imgUrl: images.about04}
-]
+import { images } from '../../constants'
+import {urlFor, client} from '../../client'
+
+// const abouts = [
+//   { title: 'Web Development', description: 'I am a good developer', imgUrl: images.about01 },
+//   { title: 'Web Design', description: 'I am a good developer', imgUrl: images.about02 },
+//   { title: 'UI/UX', description: 'I am a good developer', imgUrl: images.about03 },
+//   { title: 'Full Stack', description: 'I am a good developer', imgUrl: images.about04 }
+// ]
 
 const About = () => {
+
+  const [abouts, setAbouts] = useState([]);
+
+  useEffect(() => {
+    const query = '*[_type == "abouts"]'
+
+    client.fetch(query)
+    .then((data)=> {setAbouts(data)})
+
+
+  }, [])
+
+
+
+
   return (
     <>
       <h2 className='head-text'>
         A
         <span> Good Design </span>
-      
+
         means
         <br />
         A
@@ -33,9 +50,9 @@ const About = () => {
             key={about.title + index}
           >
 
-            <img src={about.imgUrl} alt={Image.title} />
-            <h2 className='bold-text' style={{marginTop: 20}}>{about.title}</h2>
-            <p className='p-text' style={{marginTop: 10}}>{about.description}</p>
+            <img src={urlFor(about.imgUrl)} alt={Image.title} />
+            <h2 className='bold-text' style={{ marginTop: 20 }}>{about.title}</h2>
+            <p className='p-text' style={{ marginTop: 10 }}>{about.description}</p>
 
 
 
